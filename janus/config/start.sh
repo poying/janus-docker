febaaa3795cdb1410e9a3a0acbc1467d712131e0
Po-Ying Chen <poying.me@gmail.com>
2019-08-31 17:12:13 +0800

    make port range configurable

--- janus/config/start.sh
+++ janus/config/start.sh
@@ -4,7 +4,10 @@
 sed -i -E 's/(admin_https?) *=.*/\1 = true/g' /opt/janus/etc/janus/janus.transport.http.jcfg;
 
 # use stun server
-sed -i -E 's/#stun_server *=.*/stun_server = stun.l.google.com/g' /opt/janus/etc/janus/janus.jcfg;
-sed -i -E 's/#stun_port *=.*/stun_port = 19302/g' /opt/janus/etc/janus/janus.jcfg;
+sed -i -E 's/#?stun_server *=.*/stun_server = "stun.l.google.com"/g' /opt/janus/etc/janus/janus.jcfg;
+sed -i -E 's/#?stun_port *=.*/stun_port = 19302/g' /opt/janus/etc/janus/janus.jcfg;
+
+# change rtp port range
+sed -i -E "s/#?rtp_port_range *=.*/rtp_port_range = \"${MEDIA_RTP_PORT_RANGE}\"/g" /opt/janus/etc/janus/janus.jcfg;
 
 janus --nat-1-1=$DOCKER_IP;
